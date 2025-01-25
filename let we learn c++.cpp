18b29be506b9e1b644683f834acb72608b63e009
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-25 21:38:41 +0300

    Update let we learn c++.cpp

--- let we learn c++.cpp
+++ let we learn c++.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+
+class UserAuth {
+    std::unordered_map<std::string, std::string> users;  // Stores username and password pairs
+
+public:
+    void registerUser(const std::string& username, const std::string& password) {
+        if (users.find(username) != users.end()) {
+            std::cout << "Username already exists. Please choose a different username.\n";
+        } else {
+            users[username] = password;
+            std::cout << "User registered successfully!\n";
+        }
+    }
+
+    bool loginUser(const std::string& username, const std::string& password) {
+        if (users.find(username) != users.end() && users[username] == password) {
+            std::cout << "Login successful!\n";
+            return true;
+        } else {
+            std::cout << "Invalid username or password.\n";
+            return false;
+        }
+    }
+};
 
 int main() {
+    UserAuth auth;
+    int choice;
     std::string username, password;
 
-    // Predefined username and password
-    std::string correct_username = "user";
-    std::string correct_password = "pass123";
+    while (true) {
+        std::cout << "1. Register\n2. Login\n3. Exit\nChoose an option: ";
+        std::cin >> choice;
 
-    // Prompt the user to enter username and password
-    std::cout << "Enter username: ";
-    std::cin >> username;
+        switch (choice) {
+        case 1:
+            std::cout << "Enter a username: ";
+            std::cin >> username;
+            std::cout << "Enter a password: ";
+            std::cin >> password;
+            auth.registerUser(username, password);
+            break;
+        case 2:
+            std::cout << "Enter username: ";
+            std::cin >> username;
+            std::cout << "Enter password: ";
+            std::cin >> password;
+            auth.loginUser(username, password);
+            break;
+        case 3:
+            return 0;
+        default:
+            std::cout << "Invalid option. Please try again.\n";
+        }
+    }
+}
 
-    std::cout << "Enter password: ";
-    std::cin >> password;
-
-    // Check if the entered username and password are correct
-    if 
