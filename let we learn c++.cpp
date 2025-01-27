e15b379a7afddf8455be5b692384722f6336e087
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-27 19:19:43 +0300

    Update let we learn c++.cpp

--- let we learn c++.cpp
+++ let we learn c++.cpp
@@ -5,6 +5,11 @@ int main()
 	// \t creates a horizotal table in c++ programing language
 	// \\this insert a back slash in c++ programing laguage
 	// \"this insert a double court in c++ programing language
+	char grade;
+	string name;
+	double marks;
+	float total;
+	
 	cout<<"\thello guys iam learning c++ program laguage";
 	cout<<"\n \\welcome and you also";
 	cout<<"\n \"thanks alot for you time\"";
