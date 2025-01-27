13cb7cf51982e32f829b964209279d5bcc062e5a
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-27 20:05:58 +0300

    Update Python Console.py

--- Python Console.py
+++ Python Console.py
@@ -1,4 +1,7 @@
-if 5 > 2:
- print("Five is greater than two!")  
-if 5 > 2:
-        print("Five is greater than two!") 
+if 5 > 1:
+
+    print("five si greater than ten")
+
+if 5 > 1:
+
+        print("2 is less than ten")
