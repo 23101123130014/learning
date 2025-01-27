8679844c82ff53315f963f31c4048f3c9b3cd199
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-27 19:12:45 +0300

    Update let we learn c++.cpp

--- let we learn c++.cpp
+++ let we learn c++.cpp
@@ -5,4 +5,8 @@ int main()
 	cout<<"hello guys iam learning c++ program laguage";
 	cout<<"\n welcome and you also";
 	cout<<"\n thanks alot for you time";
+	//this is a single line comment 
+	
 }
+/*this is a multiple line comment as used in c++ programing laguage
+comments are used for doatation purpose */
