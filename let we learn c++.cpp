614152c5397b33bbb1ba0f0e4bde81062a9b6526
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-28 04:50:39 +0300

    Update let we learn c++.cpp

--- let we learn c++.cpp
+++ let we learn c++.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
-using namespace std;
-int main()
+int mai()
 {
-	int day;
-	cout<<"welcome to thid new day of learning programing langauage";
-	cout<<"\nenter the number of the day: ";
-	cin>>day;
-	if (day = 1)
-	{
-		cout<<"\n it is monday today\n you are welcome to enjoy learnig c++";
-		
-	}
-	
+	int 
 }
