a7d08efa45a367ae829626110c9c738a3248bfb8
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-28 04:42:33 +0300

    Update let we learn c++.cpp

--- let we learn c++.cpp
+++ let we learn c++.cpp
@@ -2,22 +2,5 @@
 using namespace std;
 int main()
 {
-	// \t creates a horizotal table in c++ programing language
-	// \\this insert a back slash in c++ programing laguage
-	// \"this insert a double court in c++ programing language
-	char grade;
-	string name;
-	double marks;
-	float total;
-	bool reality;
-	grade ='a';
-	cout<<"\thello guys iam learning c++ program laguage";
-	cout<<"\n \\welcome and you also";
-	cout<<"\n \"thanks alot for you time\"";
-	cout<<endl;
-	cout<<grade;
-	//this is a single line comment 
-	
+	cout<<"welcome to thid new day of learning programing langauage";
 }
-/*this is a multiple line comment as used in c++ programing laguage
-comments are used for doatation purpose */
