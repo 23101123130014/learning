5fdc7c208e9586c1b92729b3aeb522f1cf8643fd
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-30 02:10:42 +0300

    Update let we learn c++.cpp

--- let we learn c++.cpp
+++ let we learn c++.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 //function declaration and definintion
 //iam adding funciton with a default parametetr here
-void thisfunction(string name = "godwin")
+void thisfunction(string name = "godwin", int age = 21 )
 {
-	cout<<"\"your name is: " <<name <<" \"\n";
+	cout<<"\"your name is: " <<name <<" " << age <<"\"\n";
 	
 	
 }
