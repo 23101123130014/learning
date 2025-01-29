8433b600f2d5f4de7b9113c11d0081630ed16f71
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-30 02:08:20 +0300

    Update let we learn c++.cpp

--- let we learn c++.cpp
+++ let we learn c++.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 //function declaration and definintion
-void thisfunction(string name)
+//iam adding funciton with a default parametetr here
+void thisfunction(string name = "godwin")
 {
 	cout<<"\"your name is: " <<name <<" \"\n";
 	
@@ -9,8 +10,10 @@ void thisfunction(string name)
 }
 int main()
 {
+	thisfunction();
 	 thisfunction("jacob");
  thisfunction("mpoki");
 	 thisfunction("Anna");
+ thisfunction();
  thisfunction("Lupakisyo");
 }
