fdc3bbad257c1b623e1e1cdcaa3237cf43822221
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-30 02:53:37 +0300

    Update let we learn c++.cpp

--- let we learn c++.cpp
+++ let we learn c++.cpp
@@ -1,11 +1,8 @@
-//a program which uses reference data type as shown below
 #include <iostream>
 using namespace std;
 int main()
 {
-string name = "jacob";
-string &jina = name;
-cout<<name<<"\n";
-cout<<jina;
+	string name = "juma";
+	cout<<&name;
+	return 0;
 }
-
