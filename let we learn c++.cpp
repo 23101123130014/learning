2ae12f6dbff27cc1e51db2ac1e308a0db6a31bbf
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-30 01:23:00 +0300

    Update let we learn c++.cpp

--- let we learn c++.cpp
+++ let we learn c++.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
+//function declaration
+void thisfunction();
 
+int main()
+{
+	thisfunction();
+}
+//function definition
+void thisfunction()
+{
+	cout<<"the function definintation is defined outside the class";
+}
