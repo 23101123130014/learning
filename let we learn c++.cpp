fe3de3c60d4474b6b53a4a667dd9aeca34cef915
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-30 03:53:18 +0300

    Update let we learn c++.cpp

--- let we learn c++.cpp
+++ let we learn c++.cpp
@@ -1,28 +1,11 @@
 #include <iostream>
 using namespace std;
-struct car{
-	int age;
-	string name;
-	string location;
-	
-};
 int main()
 {
-	car ths;
-	ths.age = 23;
-	ths.name = "tax";
-	ths.location = "mbeya";
-	cout<<ths.age<<" "<<ths.name<<" "<<ths.location<<endl;
-	
-	car thos;
-	thos.age = 12;
-	thos.name = "bajaji";
-	thos.location = "mbeya";
-	cout<<thos.age;
-	cout<<" ";
-	
-	cout<<thos.name;
-	cout<<" ";
-	cout<<thos.location;
-	return 0;
+	int i;
+	for (i = 0; i<5; i++)
+	{
+		cout<<i;
+		cout<<endl;
+	}
 }
