a036641be48098ca2198cdd22d962d6bea145c89
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-31 17:33:27 +0300

    Create substraction of two numbers.cpp

---
+++ substraction of two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,sub;
+	cout<<"\n enter the first number(minued): ";
+	cin>>a;
+	cout<<"\n enter the second number(substrahend): ";
+	cin>>b;
+	sub=a-b;
+	cout<<"the difference of the two numbers is: ";
+	cout<<sub;
+}
