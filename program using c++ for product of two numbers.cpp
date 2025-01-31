f072b7b377a8b8a4f42633fc0d4a4c49a816983a
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-31 17:48:58 +0300

    Update program using c++ for product of two numbers.cpp

--- program using c++ for product of two numbers.cpp
+++ program using c++ for product of two numbers.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main()
 {
-	int a, b, product;
-	cout<<"\n enter the first number to be included in multiplication: ";
-	cin>>a;
-	cout<<"\n ente the second number to be included in multiplication: ";
-	cin>>b;
-	product = a*b;
-	cout<<"the product of the two numbers is: ";
-	cout<<product;
+	int l,w,rea;
+	cout<"\n ente the length of the rectangle: ";
+	cin>>l;
+	cout<<"\n enter the width of the rectangle: ";
+	cin>>w;
+	rea=l*w;
+	cout<<"\n the area of the rectangle is: ";
+	cout<<rea;
+	return 0;
 }
