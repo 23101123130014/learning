dc376ec02d6f93b7ff4abca2021540ccff65ce61
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-31 17:29:57 +0300

    Create division of two numbers.cpp

---
+++ division of two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, division;
+	cout<<"\n enter the first number: ";
+	cin>>a;
+	cout<<"\n enter the second number: ";
+	cin>>b;
+	division = a/b;
+	cout<<"the division of the two numbers is: ";
+	cout<<division;
+}
