6c361e0db92666b9cb6375a94f8de11c31c01d4e
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-31 17:49:23 +0300

    Create program using c++ area of the rectangle.cpp

---
+++ program using c++ area of the rectangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int l,w,rea;
+	cout<"\n ente the length of the rectangle: ";
+	cin>>l;
+	cout<<"\n enter the width of the rectangle: ";
+	cin>>w;
+	rea=l*w;
+	cout<<"\n the area of the rectangle is: ";
+	cout<<rea;
+	return 0;
+}
