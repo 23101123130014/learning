0f2c958bc8d8d8e9cb312a1f84f94a70c0fc9a4b
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-31 17:37:03 +0300

    Create addtion of two numbers.cpp

---
+++ addtion of two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, sum;
+	cout<<"\n enter the first number to be included in addtion: ";
+	cin>>a;
+	cout<<"\n enter the third number to be included in addtion: ";
+	cin>>b;
+	sum = a + b;
+	cout<<"the sum of the two numbers is: ";
+	cout<<sum;
+}
