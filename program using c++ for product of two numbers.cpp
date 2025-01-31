437085d98e9f2f10782cd7e0d650bb1da48ccf83
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-31 17:42:07 +0300

    Create program using c++ for product of two numbers.cpp

---
+++ program using c++ for product of two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, product;
+	cout<<"\n enter the first number to be included in multiplication: ";
+	cin>>a;
+	cout<<"\n ente the second number to be included in multiplication: ";
+	cin>>b;
+	product = a*b;
+	cout<<"the product of the two numbers is: ";
+	cout<<product;
+}
