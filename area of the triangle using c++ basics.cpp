409cd27b5ae682c4539742e8273418988b34db99
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-01 21:59:27 +0300

    Create area of the triangle using c++ basics.cpp

---
+++ area of the triangle using c++ basics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float area,h,b;
+	cout<<"\n enter the base of the triangle: ";
+	cin>>b;
+	cout<<"\n enter the height of the triangle: ";
+	cin>>h;
+	area=b*h*0.5;
+	cout<<"the area is: ";
+	cout<<area;
+}
