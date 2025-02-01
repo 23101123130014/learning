18a58cc53b2a7f66b6912dbd21e2789f9be6591c
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-01 23:29:29 +0300

    Create swapping  numbers.cpp

---
+++ swapping  numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=10,b=34,c;
+	cout<<"\n the numbers before swapping";
+	cout<<"\n a= "<<a;
+	cout<<"\n b= "<<b;
+	c=a;
+	a=b;
+	b=c;
+	cout<<"\n a= "<<a;
+	cout<<"\n b= "<<b;
+}
