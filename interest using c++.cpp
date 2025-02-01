273c265fb1a7c408671c66915e1ca49d9cd6720d
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-01 22:00:34 +0300

    Create interest using c++.cpp

---
+++ interest using c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float p,r,t,interest;
+	cout<<"enter principle amount: ";
+	cin>>p;
+	cout<<"enter the value of rate: ";
+	cin>>r;
+	cout<<"enter the time: ";
+	cin>>t;
+	interest = (p*r*t)/100;	
+	cout<<"interest is: ";
+	cout<<interest;
+}
