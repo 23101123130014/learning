e69561255d988f82ab44bda26d228d2f2e65cc20
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-01 22:00:46 +0300

    Create area of the circle.cpp

---
+++ area of the circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float  radius, area;
+	cout<<"enter the radius of the triangle: ";
+	cin>>radius;
+	area = radius*3.14;
+	cout<<"area is: ";
+	cout<<area;
+	
+}
