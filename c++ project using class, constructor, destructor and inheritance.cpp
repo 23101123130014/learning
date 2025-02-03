75fd1752606b4f8cf253957ab0fa88d3687186f1
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-03 08:35:48 +0300

    Update c++ project using class, constructor, destructor and inheritance.cpp

--- c++ project using class, constructor, destructor and inheritance.cpp
+++ c++ project using class, constructor, destructor and inheritance.cpp
@@ -20,4 +20,5 @@ using namespace std;
  };
  //function prototype here
  void registerNewStudent(vector<student>& students); //function to register new student
- void showinfo(vector<student>& students)
+ void showinfo(const vector<student>& students);vector to read sudent information
+ 
