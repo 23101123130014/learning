d3a25835d7b25f2239dbcca0f9a22fc2e67b8dde
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-05 12:17:06 +0300

    Create constructorrev.cpp

---
+++ constructorrev.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class car
+{
+	public:
+		int age;
+		void display()
+		{
+			cout<<"hello jacob goddwin mwansambe";
+		}
+};
+
+int main()
+{
+	car my;
+}
