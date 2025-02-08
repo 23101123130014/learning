c584da9bdca93de9a6fbc1854545b44720ec60d1
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-08 19:54:44 +0300

    Create virtual functuion.cpp

---
+++ virtual functuion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class base
+{
+	public:
+		virtual void thiss() = 0;
+		
+};
+class chil: public base{
+	public:
+		void thiss() override{
+		cout<<"helllo";
+		}
+};
+int main()
+{
+	cout<<"\nhello the world\n";
+	
+	chil ob;
+	ob.thiss();
+}
