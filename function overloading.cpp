2c79358479812db5b9f077a30ac994293ea6a73a
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-08 19:17:48 +0300

    Create function overloading.cpp

---
+++ function overloading.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+class definition{
+	public:
+	
+	void display()
+	{
+		cout<<"\n create new entry in market";
+	}
+	void display()
+	{
+		cout<<"\n simplify market access";
+	}
+	void display()
+	{
+		cout<<"\n strenghten operation";
+	}
+	void display()
+	{
+	cout<<"\n emrove technology strenght";	
+	}
+	
+	
+};
+
+
+
+
+	int main()
+	{
+	definition ob;
+	ob.display();	
+		
+	}
+
