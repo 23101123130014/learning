2dd77b10008f7c0763f2523c9650a2837fc87e5d
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-08 19:50:06 +0300

    Update function overloading.cpp

--- function overloading.cpp
+++ function overloading.cpp
@@ -2,26 +2,67 @@
 using namespace std;
 class definition{
 	public:
+		void display()
+		{
+			cout<<"\n adding value to the product";
+		}
+		
+	};
+		
+		class seconda : public definition{
+			
+	public:
+	void display()
+	{
+		cout<<"\n you cant do everything ";
+	}
 	
+		};
+		
+		class thirda : public definition{
+	public:
 	void display()
 	{
 		cout<<"\n create new entry in market";
 	}
+		};
+		
+		class forth : public definition{
+	public:
 	void display()
 	{
 		cout<<"\n simplify market access";
 	}
+		};
+		
+		
+		class fith : public definition{
+	public:
 	void display()
 	{
 		cout<<"\n strenghten operation";
 	}
+		};
+		
+		class sixa : public definition{
+			
+	public:
 	void display()
 	{
-	cout<<"\n emrove technology strenght";	
+	cout<<"\n emprove technology strength";	
 	}
+		};
+		
+	class sevena : public definition{
+		
+public:
+	void display()
+	{
+		cout<<"\n developing finance strength";
+	}
+	};	
 	
-	
-};
+
 
 
 
@@ -29,7 +70,14 @@ class definition{
 	int main()
 	{
 	definition ob;
-	ob.display();	
+	ob.display();
+	seconda b;
+b.display();
+sevena o;
+o.display();
+forth x;
+x.display();
+	
 		
 	}
 
