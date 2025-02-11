ca1b8deab5a69816de2f3e26d5fde799d4e64faf
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-11 14:37:16 +0300

    Create c++ data types.cpp

---
+++ c++ data types.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int interger = 2;
+	char letter = 'a';
+	string text = "name";
+	bool ukweli = true;
+	bool uongo = false;
+	double desimo = 3.1212;
+	float decimal = 23.2;
+	
+	
+	cout<<"\nInteger valu "<<interger;
+	
+	cout<<"\nletter value "<<letter;
+	
+	cout<<"\nstring value"<<text;
+	
+	cout<<"\n double value "<<desimo;
+	
+	cout<<"\nflaot value " <<decimal;
+}
