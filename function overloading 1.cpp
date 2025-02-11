7f9731407232c4b8429663667a8504a25185cab8
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-11 17:46:45 +0300

    Create function overloading 1.cpp

---
+++ function overloading 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class Printer
+{
+	//acess specifier
+	public:
+		
+		void show(char g){
+			cout<<"\n grade is "<<g<<endl;
+			
+		}
+		void show(string name){
+			cout<<"\n name is "<<name<<endl;
+		}
+		void show
+}
