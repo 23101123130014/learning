38c47aee4b1d15cee7654aac46eb355367a7a77a
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-11 17:03:07 +0300

    Create constructer and destructer.cpp

---
+++ constructer and destructer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class one
+{
+public:
+int id;
+string age;
+//constructer here
+one(){
+	cout<<"constructer is code on object creation\n";
+}	
+//destructer clean up when the object is destroyed
+~one(){
+	cout<<"destructer is called on object destruction\n";
+}
+
+void display(){
+	cout<<"age is "<<age<<" and id is "<<id;
+}
+};
+
+int main()
+{
+	
+one o;	
+o.id = 2;
+o.age = 23;	
+}
