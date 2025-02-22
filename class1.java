bae556bc22c16b0d57f1a5dde3abe2eab2f12f89
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-22 22:13:56 +0300

    Create class1.java

---
+++ class1.java
@@ -0,0 +1,11 @@
+public class class1 {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("this is object oriented programing");
+    System.out.println("we are all learning object oriented programing language");
+    System.out.println("hello java object oriented programming langauage");
+    System.out.println("hell Jacob Godwin Mwansambe");
+    
+    
+  }
+}
