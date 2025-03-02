6129dcf751158e7049f7e8d2924094476bad31ff
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-02 20:23:05 +0300

    Create h3.java

---
+++ New Folder/New Folder (4)/h3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class h3
+{
+  public static void main(String [] args)
+  {
+    Scanner ob = new Scanner(System.in);
+    System.out.println("enter the value of x");
+    int x = ob.nextInt();
+    System.out.println("enter the value of y");
+    int y = ob.nextInt();
+    
+   System.out.print("is x greater than y: ");  
+   System.out.println( x == y || x > y);
+   
+    
+  }
+}
