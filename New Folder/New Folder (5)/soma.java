bd0317fe4897a964d1aa7bb47fefa98acde7f046
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-03 09:44:49 +0300

    Update soma.java

--- New Folder/New Folder (5)/soma.java
+++ New Folder/New Folder (5)/soma.java
@@ -1,1 +1,45 @@
+import java.util.Scanner;
+public class soma
+{
+  public static void main(String [] args)
+  {
+    Scanner ob = new Scanner(System.in);
+    int day = ob.nextInt();
+  
+    switch(day)
+    {
+      case 1:
+        System.out.println("Monday");
+        break;
+        
+      case 2:
+        System.out.println("Sunday");
+        break;
+      case 3:
+      System.out.println("Tuesday");
       break;
+      
+      case 4:
+        System.out.println("wednesday");
+        break;
+        
+      case 5:
+        System.out.println("thursday");
+        break;
+        
+      case 6:
+        System.out.println("friday");
+        break;
+        
+      case 7:
+        System.out.println("saturday");
+        break;
+        
+      default:
+        System.out.print("invalid choice enter another choice:__ ");
+    
+    
+    System.out.println("today is monday: ");
+    }
+  }
+}
