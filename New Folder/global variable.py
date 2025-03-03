50ab9bf98baae24b18bc6826e889c4f93d8ad9f2
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-03 10:26:50 +0300

    Create global variable.py

---
+++ New Folder/global variable.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  3 06:36:05 2025
+
+@author: JACOB MWANSAMBE
+"""
+
+x = "juma";
+
+def fun():
+   y = "local variable"
+  print("this is " + y);
+    
+fun();
+print("he");
+
+  
