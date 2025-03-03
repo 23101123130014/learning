87dee556222332c41d0d3319442bc90d2e5e5f81
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-03 09:45:12 +0300

    Create ty.py

---
+++ New Folder/ty.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  3 06:44:05 2025
+
+@author: JACOB MWANSAMBE
+"""
+
+x = 34;
+y = 2.3;
+c = 1j;
+
+print(type(x));
+print(type(y));
+print(type(c));
