3b4a95ef8a85dee3c61944c8b95b7fd398b147c9
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-03 10:25:40 +0300

    Create variable outside the function used inside the function.py

---
+++ New Folder/variable outside the function used inside the function.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  3 06:23:39 2025
+
+@author: JACOB MWANSAMBE
+"""
+
+x = "awesome";
+z = "juma";
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+
+print("python is " + x);
+print("hello " + z);
