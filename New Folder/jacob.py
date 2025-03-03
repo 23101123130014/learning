40e6cfcfa3b9ed014fc8feac8d56a23d17029599
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-03 09:45:06 +0300

    Create jacob.py

---
+++ New Folder/jacob.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  3 06:02:35 2025
+
+@author: JACOB MWANSAMBE
+"""
+
+if(5 > 2):
+   print("hello jacob");
+print("hello jacob");   
+#this is a comment as used in python
+#you can impose the use of multiple line comment
+"""
+this can be mposed as multiple line comment in python
+do you no all about this welcome
+to the wolrd of python here
+"""
