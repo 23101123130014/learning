540f853a7f0d89ff2b68220186731abc5da6a662
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-03 10:25:43 +0300

    Update variable in python.py

--- New Folder/variable in python.py
+++ New Folder/variable in python.py
@@ -1,12 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  3 06:10:47 2025
 
+@author: JACOB MWANSAMBE
+"""
 
+x = "juma";
 y = 23;
+print(x);
 print(y);
 
 z = str(3);
+x = int(3);
 c = float(3);
 
 
+print("the vaku of the above variable are");
 print(z);
 print(x);
 print(c);
