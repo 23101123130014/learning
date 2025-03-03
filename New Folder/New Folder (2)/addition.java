ad66a39a2c4239907b782ab36fd6d05a7bc3dafc
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-03 09:45:02 +0300

    Create addition.java

---
+++ New Folder/New Folder (2)/addition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class addition
+{
+ 
+  public static void main(String [] args)
+  {
+    Scanner on = new Scanner();
+     int a = 23;
+  int b = 2;
+  int sum = a + b;
+    System.out.println("let we work out with calculation");
+    System.out.println("sum is: " + sum);
+    
+   
+   
+   
+  }
+}
