692132b7745effcfd1b3e55624812a4384ac8b0e
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-06 17:24:02 +0300

    Update rock paper scissor.py

--- pythonProject2/rock paper scissor.py
+++ pythonProject2/rock paper scissor.py
@@ -1,2 +1,5 @@
 import random
 user_choice = int(input("enter your choice, type 0 for rock, 1 for paper, 2 for Scissoer"))
+computer_choice = random.randint(0, 2);
+print("computer choice")
+print(computer_choice)
