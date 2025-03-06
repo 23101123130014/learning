0135a9143da83ed81d52155dca08ba4db48b299d
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-06 14:45:21 +0300

    Create hy.java

---
+++ New Folder/New Folder (6)/hy.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class hy
+{
+  public static void main(String [] args)
+  {
+    Scanner ob = new Scanner(System.in);
+    System.out.println("what is your name ");
+    String name = ob.nextLine();
+    System.out.println("your name is " + name);
+  }
+}
+    
