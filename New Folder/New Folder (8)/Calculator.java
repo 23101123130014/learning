f3a5ce62af991d50986ffd9de1b8da4219eeb93f
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-12 11:11:24 +0300

    Update Calculator.java

--- New Folder/New Folder (8)/Calculator.java
+++ New Folder/New Folder (8)/Calculator.java
@@ -21,6 +21,7 @@ public class Calculator {
         if (operand2 == 0) {
             throw new ArithmeticException("Cannot modulo by zero");
         return operand1 % operand2;
+    }
 
     public static void main(String[] args) {
         double num1 = 10.0;
