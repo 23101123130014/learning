22245a3bdaeecbeead53676b6125977af916c652
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-16 08:19:08 +0300

    Update Variable1.java

--- New Folder/New Folder (11)/Variable1.java
+++ New Folder/New Folder (11)/Variable1.java
@@ -3,6 +3,7 @@
     
     public static void main(String [] args)
     {
+        //for static varible the default value to it it is o
 public static void main(String [] args)
 {
     System.out.println(" hello dear");
