9e4ee969c6e9a09e809f1383671fa3e410c3d487
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-16 08:19:32 +0300

    Update Variable1.java

--- New Folder/New Folder (11)/Variable1.java
+++ New Folder/New Folder (11)/Variable1.java
@@ -6,6 +6,7 @@
         System.out.println("let wwe learn about variable in java object oriented programming");
         //for static varible the default value to it it is o
         System.out.print("default value for static variable ");
+        System.out.println(age);
 public static void main(String [] args)
 {
     System.out.println(" hello dear");
