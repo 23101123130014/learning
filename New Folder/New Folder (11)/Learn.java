063db1b7f6b906fb3d80b30a4dba987a914e876a
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-17 22:38:43 +0300

    Update Learn.java

--- New Folder/New Folder (11)/Learn.java
+++ New Folder/New Folder (11)/Learn.java
@@ -3,6 +3,7 @@
   // a constructer is ti be wrritten here
   public Learn()
     
+    System.out.println("the information for the first student are ");
     p1.display();
     
     System.out.println("the information for the second student are");
