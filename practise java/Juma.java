1959b20ed4ee0585ed27218fbb8779d12c7e970d
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-18 14:02:58 +0300

    Create Juma.java

---
+++ practise java/Juma.java
@@ -0,0 +1,13 @@
+public class Juma
+{
+  public static void main(String [] args)
+  {
+    System.out.println("hello the world");
+    System.out.print("what are ");
+    System.out.println("learning");
+    System.out.print("this is all about java object oriented programming language\n");
+    System.out.print("hello it funny");
+    System.out.print(" \n o learn java");
+    
+  }
+}
