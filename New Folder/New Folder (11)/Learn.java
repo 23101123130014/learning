41d7a369a90ec66cd48a593d49a9908bd4046c7d
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-18 14:06:26 +0300

    Update Learn.java

--- New Folder/New Folder (11)/Learn.java
+++ New Folder/New Folder (11)/Learn.java
@@ -1,7 +1,56 @@
+import java.util.Scanner;
+public class Learn
+{
+  int id = 1;
+  String name;
+  
+  void display()
+  {
+    System.out.println("---student detail here---");
     
+    System.out.println("name " + name);
+    System.out.println("id " + id);
+}
+  static void welcome()
+  {
+    System.out.println("your welcome to our system essentialy for the management of stdent information");
+  }
+  
+  public void welcome2()
+  {
+    System.out.println("good journey");
+  }
   
   // a constructer is ti be wrritten here
   public Learn()
+  {
+    System.out.println("this is a constructer");
+    name = "jacob";
+  }
+  public static void main(String [] args)
+  {
+    
+    welcome();
+     Learn oo = new Learn();
+    oo.welcome2();
+    Scanner ob = new Scanner(System.in);
+    //object of class learn
+   
+    Learn p1 = new Learn();
+    Learn p2 = new Learn();
+    
+    System.out.print("enter the id of the first person ");
+   p1.id = ob.nextInt();
+   ob.nextLine();
+   
+    System.out.println("enter the name of the first student");
+    p1.name = ob.nextLine();
+   
+    
+    System.out.print("enter the id of the second person ");
+    p2.id = ob.nextInt();
+    ob.nextLine();
+    
     System.out.println("enter the name of the second student ");
     p2.name = ob.nextLine();
     
