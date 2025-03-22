cb0d1d1887adf75d976e7008e4cda86aacc914c8
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-22 19:48:48 +0300

    Create Beson.java

---
+++ New Folder/New Folder (11)/Beson.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+
+public class Beson
+{
+  public static void main(String [] args)
+    
+  {
+    LinkedList<String> magari = new LinkedList<String>();
+    magari.add("Jacob godwin");
+    magari.add("Alfa pokera");
+    magari.add("Merry tiluli");
+    magari.add("johnson nchombo");
+ magari.addFirst("Saidi ali");
+    for(String s: magari)
+    {
+      System.out.println(s);
+    }
+  }
+}
+    
