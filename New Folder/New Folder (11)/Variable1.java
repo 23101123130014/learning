81b232ff2111688a1dc1588b69d66dc4b25e0e8d
23101123130014 <jacobmwansambe1@gmail.com>
2025-04-09 00:01:38 +0300

    Update Variable1.java

--- New Folder/New Folder (11)/Variable1.java
+++ New Folder/New Folder (11)/Variable1.java
@@ -1,26 +1,5 @@
-     System.out.println(c);
-    }
-    
-    public static void main(String [] args)
-    {
-        System.out.println("let wwe learn about variable in java object oriented programming");
-        //for static varible the default value to it it is o
-        System.out.print("default value for static variable ");
-        System.out.println(age);
-        
-        //also for integer value the default value for it it is zero
-        
-        Variable1 object1 = new Variable1();
-        System.out.print("default value for interger ");
-        object1.display();
-    }
+public class Oak{
+  public static void main(String [] args){
+    System.out.println("hello world");
+  }
 }
-
-/*
-class you{
-public static void main(String [] args)
-{
-    System.out.println(" hello dear");
-}
-}
-*/
