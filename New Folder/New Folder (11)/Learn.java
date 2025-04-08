d0a753bc0d83ab0d2ad007d536c556ae75785699
23101123130014 <jacobmwansambe1@gmail.com>
2025-04-09 00:02:53 +0300

    Update Learn.java

--- New Folder/New Folder (11)/Learn.java
+++ New Folder/New Folder (11)/Learn.java
@@ -1,72 +1,52 @@
-import java.util.Scanner;
-public class Learn
-{
-  int id = 1;
-  String name;
-  
-  void display()
-  {
-    System.out.println("---student detail here---");
-    
-    System.out.println("name " + name);
-    System.out.println("id " + id);
+
+
+public class Learn{
+  public static void main(String [] args){
+   String text = "abcdefghijklmpqrstuvwxyz";
+   String text2 = "HELLO JUMA";
+  String name  = "jacob";
+   String lname = "mwansambe";
+    System.out.println("the lenght of the text is "+ text.length());
+    System.out.println(text.toUpperCase());
+    System.out.println(text2.toLowerCase());
+    System.out.println(text.indexOf("cd"));
+    System.out.println(name.concate(lname));
+  }
 }
-  static void welcome()
+/*
+public class Learn{
+  public static void main(String [] args){
+    String text = "abcdefghijklmpqrstuvwxyz";
+    System.out.println("the lenght of the text is "+ text.length());
+    }
+}
+*/
+/*
+public class Learn{
+  public static void main(String[] args)
   {
-    System.out.println("your welcome to our system essentialy for the management of stdent information");
-  }
-  
-  public void welcome2()
-  {
-    System.out.println("good journey");
-  }
-  
-  // a constructer is ti be wrritten here
-  public Learn()
-  {
-    System.out.println("this is a constructer");
-    name = "jacob";
-  }
+    double a = 34.67d;
+    int b = (int) a;
+    System.out.println("the value of double is " + a);
+    System.out.println(b);
+  }}
+  */
+/*
+public class Learn{
+  public static void main(String [] args){
+    int a = 23;
+    double b = a;
+System.out.println(b);
+  }}
+*/
+/*
+public class Learn{
+  public static void main(String[] args){
+    System.out.println(3 + 4);
+  }}
+  */
+/*public class Learn{
   public static void main(String [] args)
   {
-    
-    welcome();
-     Learn oo = new Learn();
-    oo.welcome2();
-    Scanner ob = new Scanner(System.in);
-    //object of class learn
-   
-    Learn p1 = new Learn();
-    Learn p2 = new Learn();
-    
-    System.out.print("enter the id of the first person ");
-   p1.id = ob.nextInt();
-   ob.nextLine();
-   
-    System.out.println("enter the name of the first student");
-    p1.name = ob.nextLine();
-   
-    
-    System.out.print("enter the id of the second person ");
-    p2.id = ob.nextInt();
-    ob.nextLine();
-    
-    System.out.println("enter the name of the second student ");
-    p2.name = ob.nextLine();
-    
-    
-    System.out.println("the id of the first student is " + p1.id);
-    System.out.println("the id of the second student is " + p2.id);
-    
-    System.out.println("the information for the first student are ");
-    p1.display();
-    
-    System.out.println("the information for the second student are");
-    p2.display();
-    
-   // Fyear on = new Fyear();
-    //on.name = "jacob";    
-    
-    
-  }
-}
+    System.out.println("hello what is this");
+  }}*/
