3310dac4f22239a731a8566523b7c5e1156fd72e
23101123130014 <jacobmwansambe1@gmail.com>
2025-04-09 00:00:42 +0300

    Update Main.java

--- New Folder/New Folder (11)/Main.java
+++ New Folder/New Folder (11)/Main.java
@@ -1,12 +1,12 @@
-import java.util.ArrayList;
 
-public class Main { 
-  public static void main(String[] args) { 
-    ArrayList<String> cars = new ArrayList<String>();
-    cars.add("Volvo");
-    cars.add("BMW");
-    cars.add("Ford");
-    cars.add("Mazda");
-    System.out.println(cars);
-  } 
-}
+
+
+
+
+
+
+/*public class Main{
+  public static void main(String [] args)
+  {
+    System.out.println("hello what is this");
+  }}*/
