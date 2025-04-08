90fc18e37d74620af0d4e5c3e52c8e728b3b3d9f
23101123130014 <jacobmwansambe1@gmail.com>
2025-04-09 00:06:51 +0300

    Update joseph.cpp

--- joseph.cpp
+++ joseph.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string name;
+	int history;
+	int biology;
+	char average;
+	int total;
+	 cout <<"\n What is you name: ";
+	 cin>>name;
+	 cout<<"Your name is: ";
+	 cout<<name;
+	 cout<<"\nenter you history marks: ";
+	 cin>>history;
+	 cout<<"Enter you biology marks ";
+	 cin>>biology;
+	 total = biology + history;
 	 cout<<"\n average is ";
+	 cout<<total;
+	 average
+	 
+	 
+	 
+}
